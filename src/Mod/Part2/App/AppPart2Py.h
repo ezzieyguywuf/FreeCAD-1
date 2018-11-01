fa6fc7b67aa7bccc7b38195b0663dd53e53c7de0
ezzieyguywuf <WolfgangESanyer@gmail.com>
2018-10-31 22:26:54 -0400

    Added Part2 workbench. Doesn't do anything yet.

---
+++ src/Mod/Part2/App/AppPart2Py.h
@@ -0,0 +1,13 @@
+#ifndef AppPart2Py
+#define AppPart2Py
+
+#include <pybind11/pybind11.h>
+
+#include <Mod/Part/App/TopoShapePy.h>
+#include <Mod/Part/App/TopoShapePy.cpp>
+
+#include <OccShape.h>
+namespace Part2{
+
+}
+#endif /* ifndef AppPart2Py */
